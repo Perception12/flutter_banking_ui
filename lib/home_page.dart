a0be3e25b86296b5c69a14143ba11caa699ede02
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-11-21 17:50:34 +0100

    separate homepage file from main file

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
